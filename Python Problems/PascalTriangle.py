14ef66defff56d8cb15bb2f3d352f6bd5a7c4e72
NihalSharma <sharmanihal96@gmail.com>
2022-10-10 14:20:32 +0530

    Oct 10

--- Python Problems/PascalTriangle.py
+++ Python Problems/PascalTriangle.py
@@ -2,22 +2,16 @@ from typing import List
 
 
 class Solution:
-    def __init__(self) -> None:
-        self.res=[]
     def generate(self, numRows: int) -> List[List[int]]:
-        if numRows>2:
-            self.res.append([1])
-            self.res.append([1,1])
-        elif numRows==2:
-            return [[1],[1,1]]
-        elif numRows==1:
-            return [1]
-        self.solve(numRows)
-        return self.res
-    def solve(self,numRows):
         if numRows==1:
+            return [1,1]
+        elif numRows==0:
             return [1]
-        if numRows==2:
+        return self.solve(numRows)
+    def solve(self,numRows):
+        if numRows==0:
+            return [1]
+        if numRows==1:
             return [1,1]
         arr=self.solve(numRows-1)
         i=0
@@ -28,10 +22,9 @@ class Solution:
             i=i+1
             j=j+1
         nums.append(1)
-        self.res.append(nums)
         return nums
 
 ans= Solution()
-print(ans.generate(14))
+print(ans.generate(2))
             
             
