ec8687e5f2cd2b35b6a44306a3780cb20602eb1d
NihalSharma <sharmanihal96@gmail.com>
2022-10-14 12:10:20 +0530

    Oct 14

--- Python Problems/56.MergerArray.py
+++ Python Problems/56.MergerArray.py
@@ -2,22 +2,23 @@ from typing import List
 
 
 class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+    def merge(self, intervals: List[List[int]]):
         i=0
-        n=len(intervals)
+        count=0
+        m=n=len(intervals)
         intervals.sort(key=lambda x: (x[0]))
         while i<n-1:
-            if intervals[i][1]>=intervals[i+1][0] and intervals[i][0]<=intervals[i+1][1]:
-                ele= min(intervals[i][0],intervals[i+1][1])
-                ele2= max(intervals[i][1],intervals[i+1][1])
-                intervals.pop(i)
-                intervals.pop(i)
-                intervals.insert(i,[ele,ele2])
+            if intervals[i][1]>intervals[i+1][0] and intervals[i][0]<=intervals[i+1][1]:
+                if intervals[i][1]>intervals[i+1][1]:
+                    intervals.pop(i)
+                else:
+                    intervals.pop(i+1)
+                count=count+1
                 n=n-1
             else:
                 i=i+1
-        return intervals
+        return count
 
 ans= Solution()
-print(ans.merge( [[2,3],[4,5],[6,7],[8,9],[1,10]]))
+print(ans.merge( [[1,100],[11,22],[1,11],[2,12]]))
         
