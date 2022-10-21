fe6c09331ad2425f6843bc61ba23915e99f26e84
NihalSharma <sharmanihal96@gmail.com>
2022-10-21 12:46:00 +0530

    oct 21

--- Python Problems/HouseRobber2.py
+++ Python Problems/HouseRobber2.py
@@ -1,19 +1,22 @@
 from typing import List
 
 
+##Memoization 
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        memo1=[-1 for i in range(len(nums)-1)]
-        memo2=[-1 for i in range(len(nums)-1)]
-        res1= self.solve(nums[0:len(nums)-1],len(nums)-2,memo1)
-        res2= self.solve(nums[1     :],len(nums)-2,memo2)
-        return max(res1,res2)
-    def solve(self,nums,n,memo):
-        if n<0:
-            return 0
-        if memo[n]!=-1:
-            return memo[n]
-        memo[n]= max(nums[n]+self.solve(nums,n-2,memo),self.solve(nums,n-1,memo))
-        return memo[n]
+        if len(nums)==1:
+            return nums[0]
+        memo1=[-1 for i in range(len(nums))]
+        memo2=[-1 for i in range(len(nums))]
+        res1= self.solve(nums[0:len(nums)-1],memo1)
+        res2= self.solve(nums[1:],memo2)
+        return max(res1, res2)
+    def solve(self,nums,memo):
+        memo[0]=0
+        memo[1]=nums[0]
+        for i in range(2, len(memo)):
+            memo[i]= max(nums[i-1]+memo[i-2],memo[i-1])
+        return memo[len(memo)-1]
+    
 ans= Solution()
 print(ans.rob([1,2,3,1]))
