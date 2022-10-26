7f3e450592637cc9cf519dd9937ff74cb8e5aeae
NihalSharma <sharmanihal96@gmail.com>
2022-10-26 13:07:47 +0530

    Oct 26

--- Python Problems/MaxInSubarray.py
+++ Python Problems/MaxInSubarray.py
@@ -3,11 +3,14 @@ import sys
 
 class Solution:
     def max(self,arr):
+        if len(arr)==2:
+            return max(arr[0]*arr[1], arr[1], arr[0])
         return self.solve(arr,1)
     def solve(self,arr,product):
         if len(arr)==0:
             return product
-        return max(self.solve(arr[0:len(arr)-1],product*arr[len(arr)-1]),self.solve(arr[0:len(arr)-1],1))
-    
+        res1= self.solve(arr[0:len(arr)-1],product*arr[len(arr)-1])
+        res2= self.solve(arr[0:len(arr)-1],arr[len(arr)-1])
+        return max (res1,res2)
 ans= Solution()
-print(ans.max( [-2,0,-1]))
+print(ans.max( [3,-1,4]))
