da8e7af591dfa857f7c5cd622f14b47a6d88cd94
NihalSharma <sharmanihal96@gmail.com>
2022-11-03 12:13:46 +0530

    Nov 3

---
+++ Python Problems/Word Break.py
@@ -0,0 +1,19 @@
+from audioop import reverse
+from math import fabs
+from typing import List
+
+
+class Solution:
+    def wordBreak(self,s, words):
+        d = [False] * len(s)    
+        for i in range(len(s)):
+            for w in words:
+                if d[i]:
+                    break
+                if  (d[i-len(w)] or i-len(w) == -1) and w == s[i-len(w)+1:i+1] :
+                    d[i] = True
+        return d[-1]
+
+ans= Solution()
+print(ans.wordBreak("cars"
+,["car","ca","rs"]))
